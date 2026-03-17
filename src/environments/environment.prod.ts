3733da35da92e374a86482e60e565510ef54a4a0
Hook <73350763+Hooklamat0r@users.noreply.github.com>
2026-03-17 21:09:55 +0100

    Update apiUrl in environment.prod.ts
    Removed trailing slash from apiUrl in production environment.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://marvelrivalsback.onrender.com/api/',
+  apiUrl: 'https://marvelrivalsback.onrender.com/api',
   marvelRivalsApiKey: '4e55500b750bf30512a7c4964b078b84327e5ccd492b08a2c1458509eeb9a857'
 };
